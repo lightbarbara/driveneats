bee3c83af4c03c7b4abf9154193b8d0c9e4f77ae
lightbarbara13 <barbaraluz@eq.ufrj.br>
2022-06-12 15:05:47 -0300

    selecao dos produtos

---
+++ scripts.js
@@ -0,0 +1,66 @@
+function destaque1() {
+    let elemento = document.querySelector('.opcao1');
+    elemento.classList.toggle('destaque');
+    let icone = document.querySelector('.opcao1 ion-icon');
+    icone.classList.toggle('desaparecer')
+}
+
+function destaque2() {
+    let elemento = document.querySelector('.opcao2');
+    elemento.classList.toggle('destaque');
+    let icone = document.querySelector('.opcao2 ion-icon');
+    icone.classList.toggle('desaparecer')
+}
+
+function destaque3() {
+    let elemento = document.querySelector('.opcao3');
+    elemento.classList.toggle('destaque');
+    let icone = document.querySelector('.opcao3 ion-icon');
+    icone.classList.toggle('desaparecer')
+}
+
+function destaque4() {
+    let elemento = document.querySelector('.opcao4');
+    elemento.classList.toggle('destaque');
+    let icone = document.querySelector('.opcao4 ion-icon');
+    icone.classList.toggle('desaparecer')
+}
+
+function destaque5() {
+    let elemento = document.querySelector('.opcao5');
+    elemento.classList.toggle('destaque');
+    let icone = document.querySelector('.opcao5 ion-icon');
+    icone.classList.toggle('desaparecer')
+}
+
+function destaque6() {
+    let elemento = document.querySelector('.opcao6');
+    elemento.classList.toggle('destaque');
+    let icone = document.querySelector('.opcao6 ion-icon');
+    icone.classList.toggle('desaparecer')
+}
+
+function destaque7() {
+    let elemento = document.querySelector('.opcao7');
+    elemento.classList.toggle('destaque');
+    let icone = document.querySelector('.opcao7 ion-icon');
+    icone.classList.toggle('desaparecer')
+}
+
+function destaque8() {
+    let elemento = document.querySelector('.opcao8');
+    elemento.classList.toggle('destaque');
+    let icone = document.querySelector('.opcao8 ion-icon');
+    icone.classList.toggle('desaparecer')
+}
+
+function destaque9() {
+    let elemento = document.querySelector('.opcao9');
+    elemento.classList.toggle('destaque');
+    let icone = document.querySelector('.opcao9 ion-icon');
+    icone.classList.toggle('desaparecer')
+}
+
+function contar() {
+
+}
